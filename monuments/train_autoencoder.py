4b8f31f92c944e7291db85d84e642a5aa595def3
cyril Poulet <cyril.poulet@centraliens.net>
2018-03-30 23:33:44 +0200

    autoencoder export fixé

--- monuments/train_autoencoder.py
+++ monuments/train_autoencoder.py
@@ -5,25 +5,29 @@ import tensorflow as tf
 from tensorflow.python.estimator.model_fn import ModeKeys as Modes
 from tensorflow.contrib.training.python.training import hparam
 from tensorflow.python.lib.io import file_io
+from tensorflow.contrib.learn.python.learn.utils import input_fn_utils
+
+
+feature_spec = {
+    'image': tf.FixedLenFeature([], tf.string),
+    'key': tf.FixedLenFeature([1], tf.string),
+    'height': tf.FixedLenFeature([1], tf.int64),
+    'width': tf.FixedLenFeature([1], tf.int64),
+    'image_shape': tf.FixedLenFeature([3], tf.int64)
+}
 
 
 def parse_example(serialized_example):
     features = tf.parse_single_example(
         serialized_example,
         # Defaults are not specified since both keys are required.
-        features={
-            'image': tf.FixedLenFeature([], tf.string),
-            'key': tf.FixedLenFeature([1], tf.string),
-            'height': tf.FixedLenFeature([1], tf.int64),
-            'width': tf.FixedLenFeature([1], tf.int64),
-            'image_shape': tf.FixedLenFeature([3], tf.int64)
-        })
+        features=feature_spec)
     image = tf.decode_raw(features['image'], tf.float32)
     key = features['key']
     height = tf.cast(features['height'], tf.int32)
     width = tf.cast(features['width'], tf.int32)
     image = tf.reshape(image, [8, 8, 1536])
-    return (image, height, width), key
+    return (image, height, width, key), features["image_shape"]
 
 
 def input_fn(filenames, num_epochs=None):
@@ -43,7 +47,10 @@ def generate_model_fn(base_block_size=2,
 
     def _model_fn(mode, features, labels):
 
-        image, height, width = features
+        if mode == Modes.PREDICT:
+            image, height, width, key = features['image'], features['height'], features['width'], features['key']
+        else:
+            image, height, width, key = features
         blocks = []
 
         # Extract descriptor blocks
@@ -107,7 +114,7 @@ def generate_model_fn(base_block_size=2,
         if mode == Modes.PREDICT:
             # Convert predicted_indices back into strings
             descriptors = {
-                'key': labels,
+                'key': key,
                 'descriptors': tf.greater(tf.nn.sigmoid(output), tf.constant(0.5))
             }
             export_outputs = {
@@ -173,22 +180,25 @@ def generate_model_fn(base_block_size=2,
     return _model_fn
 
 
-def example_serving_input_fn():
+def serving_input_fn():
     """Build the serving inputs."""
     example_bytestring = tf.placeholder(
-        shape=[None],
-        dtype=tf.string,
+        shape=None,
+        dtype=tf.string
     )
-    features = parse_example(example_bytestring)
-    return tf.estimator.export.ServingInputReceiver(features, {'example_proto': example_bytestring})
+    (image, height, width, key), _ = parse_example(example_bytestring)
+    return tf.estimator.export.ServingInputReceiver({"image": image, "width": width, "height": height, "key": key},
+                                                    {'example_proto': example_bytestring})
 
 
 def run_experiment(hparams):
     """Run the training and evaluate using the high level API"""
 
+    file_io.recursive_create_dir(hparams.job_dir)
+
     train_tfrecords = [os.path.join(hparams.train_dir, f) for f in file_io.list_directory(hparams.train_dir)
                        if 'tfrecords' in f]
-
+    tf.logging.info('Detected {} training files'.format(len(train_tfrecords)))
     train_input = lambda: input_fn(
         train_tfrecords,
         num_epochs=hparams.num_epochs
@@ -197,15 +207,15 @@ def run_experiment(hparams):
     # Don't shuffle evaluation data
     eval_tfrecords = [os.path.join(hparams.eval_dir, f) for f in file_io.list_directory(hparams.eval_dir)
                        if 'tfrecords' in f]
-
+    tf.logging.info('Detected {} input files'.format(len(eval_tfrecords)))
     eval_input = lambda: input_fn(eval_tfrecords)
 
+    tf.logging.info('parameters: {}'.format(hparams.to_json()))
     train_spec = tf.estimator.TrainSpec(train_input,
                                         max_steps=hparams.train_steps
                                         )
 
-    exporter = tf.estimator.FinalExporter('auto_encoder',
-                                          example_serving_input_fn)
+    exporter = tf.estimator.FinalExporter('auto_encoder', serving_input_fn)
     eval_spec = tf.estimator.EvalSpec(eval_input,
                                       steps=hparams.eval_steps,
                                       exporters=[exporter],
@@ -261,10 +271,9 @@ if __name__ == "__main__":
         "train_dir": args.train_dir,
         "num_epochs": 10,
         "eval_dir": args.eval_dir,
-        "train_steps": 100,
-        "eval_steps": 100,
-        'throttle_secs': 60,
-
+        "train_steps": 1,
+        "eval_steps": 1,
+        'throttle_secs': 150,
         "base_block_size": 2,
         "convs_nb_kernels": 128,
         "convs_fc_size": 612,
